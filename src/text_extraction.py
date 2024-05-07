2d13925be618f8d87378b83deaa823f0521a737c
MasakiOkamura <tukuoka7@gmail.com>
2024-05-07 19:36:38 +0900

    debug : タグの除去方法及びテキスト抽出方法修正

--- src/text_extraction.py
+++ src/text_extraction.py
@@ -1,5 +1,12 @@
 import xml.etree.ElementTree as ET
 
+import re
+
+def remove_vacant_brackets(text):
+    text = re.sub(r'\[(?![^\[\]]*[a-zA-Z])[^\[\]]*\]', '', text)
+    text = re.sub(r'\((?![^\(\)]*[a-zA-Z])[^\(\)]*\)', '', text)
+    return text
+
 def generate_record(xml_string):
     abstract_text, body_text = extract_abstract_and_body_text(xml_string)
     return f"{abstract_text}\n\n{body_text}".strip()
@@ -9,8 +16,8 @@ def extract_abstract_and_body_text(xml_string):
         root = ET.fromstring(xml_string)
         abstract_section = root.find('.//front/article-meta/abstract')
         body_section = root.find('.//body')
-        abstract_text = xml_to_plain_text(ET.tostring(abstract_section, encoding='unicode')) if abstract_section is not None else ""
-        body_text = xml_to_plain_text(ET.tostring(body_section, encoding='unicode')) if body_section is not None else ""
+        abstract_text = (ET.tostring(abstract_section, encoding='unicode')) if abstract_section is not None else ""
+        body_text = xml_to_clean_plain_text(ET.tostring(body_section, encoding='unicode'),['xref','fig','table-wrap','inline-formula','disp-formula']) if body_section is not None else ""
         return abstract_text, body_text
     except ET.ParseError as e:
         print(f"Failed to parse XML: {e}")
@@ -43,3 +50,56 @@ def xml_to_plain_text(xml_string):
         plain_text += process_element(child)
 
     return plain_text.strip()
+
+def xml_to_clean_plain_text(xml_string,remove_list):
+    # 削除するタグ一覧
+    # xref
+    # fig
+    # table-wrap
+    # inline-formula
+    # disp-formula
+    
+    def process_element(element,remove_list):
+        text = ""
+        if element.tag == 'xref':                
+            return text
+        elif  element.tag == 'fig':            
+            return text
+        elif  element.tag == 'inline-formula' :                  
+            return text
+        elif  element.tag == 'disp-formula':
+            return text
+        elif   element.tag == 'table-wrap':            
+            return text
+        else :
+            text += " ".join(update_itertext(element,remove_list)).strip()            
+
+        return text
+
+    root = ET.fromstring(xml_string)
+    plain_text = ""
+    for child in root:
+        plain_text += process_element(child,remove_list)
+
+    return remove_vacant_brackets(plain_text.strip().replace("\n", " ") )
+
+def update_itertext(self,remove_list):
+    """Create text iterator.
+
+    The iterator loops over the element and all subelements in document
+    order, returning all inner text.
+
+    """
+    tag = self.tag
+    if not isinstance(tag, str) and tag is not None:
+        return
+    t = self.text
+    if t:
+        yield t
+    for e in self:
+		    # 取り除きたいタグのリストで選別
+        if not e.tag in remove_list:
+            yield from update_itertext(e,remove_list)
+        t = e.tail
+        if t:                
+            yield t
